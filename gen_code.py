cb3ebc506b46c2c19d32b4c017e36d767645a127
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2020-12-15 02:04:39 +0600

    stage_3 rev

--- gen_code.py
+++ gen_code.py
@@ -14,8 +14,8 @@ class CodeGenerator:
             self._traverse(node.statement)
         elif isinstance(node, Return):
             self._traverse(node.val)
-            print(f"\tpopl %eax", file=self.out_file)
-            print("\tret", file=self.out_file)
+            self.asm_print(f"\tpopl %eax")
+            self.asm_print("\tret")
         elif isinstance(node, Expression):
             self._traverse(node.expr)
         elif isinstance(node, UnaryOperator):
@@ -32,34 +32,37 @@ class CodeGenerator:
         self._traverse(self.ast_root)
 
     def _generate_function_asm(self, name):
-        print(f"\t.globl {name}", file=self.out_file)
-        print(f"{name}:", file=self.out_file)
+        self.asm_print(f"\t.globl {name}")
+        self.asm_print(f"{name}:")
 
     def _generate_const_asm(self, val):
-        print(f"\tpushl ${val}\n", file=self.out_file)
+        self.asm_print(f"\tpushl ${val}\n")
 
     def _generate_unary_op_asm(self, operator_name):
-        print(f"\tpopl %eax", file=self.out_file)
+        self.asm_print(f"\tpopl %eax")
         if operator_name == "Negation":
-            print(f"\tneg %eax", file=self.out_file)
+            self.asm_print(f"\tneg %eax")
         elif operator_name == "Bitwise complement":
-            print(f"\tnot %eax", file=self.out_file)
+            self.asm_print(f"\tnot %eax")
         else:  # Logical Negation
-            print(f"\tcmpl $0, %eax", file=self.out_file)  # this will set zero-flag(ZF) in to 1 if eax == 0
-            print(f"\tmovl $0, %eax", file=self.out_file)  # set eax to 0 means all bytes of eax is set to 0
-            print(f"\tsete %al", file=self.out_file)  # 'sete' sets al to 1 if ZF is 1
-        print(f"\tpushl %eax\n", file=self.out_file)
+            self.asm_print(f"\tcmpl $0, %eax")  # this will set zero-flag(ZF) in to 1 if eax == 0
+            self.asm_print(f"\tmovl $0, %eax")  # set eax to 0 means all bytes of eax is set to 0
+            self.asm_print(f"\tsete %al")  # 'sete' sets al to 1 if ZF is 1
+        self.asm_print(f"\tpushl %eax\n")
 
     def _generate_binary_op_asm(self, operator_name):
-        print("\tpopl %ecx /* right_operand */", file=self.out_file)
-        print("\tpopl %eax /* left_operand */", file=self.out_file)
+        self.asm_print("\tpopl %ecx /* right_operand */")
+        self.asm_print("\tpopl %eax /* left_operand */")
         if operator_name == "Addition":
-            print("\taddl %ecx, %eax", file=self.out_file)
+            self.asm_print("\taddl %ecx, %eax")
         elif operator_name == "Negation":
-            print("\tsubl %ecx, %eax", file=self.out_file)
+            self.asm_print("\tsubl %ecx, %eax")
         elif operator_name == "Multiplication":
-            print("\timul %ecx, %eax", file=self.out_file)
+            self.asm_print("\timul %ecx, %eax")
         elif operator_name == "Division":
-            print("\tcdq", file=self.out_file)
-            print("\tidivl %ecx", file=self.out_file)
-        print("\tpushl %eax /* push the result in stack */\n", file=self.out_file)
+            self.asm_print("\tcdq")
+            self.asm_print("\tidivl %ecx")
+        self.asm_print("\tpushl %eax /* push the result in stack */\n")
+
+    def asm_print(self, asm_code):
+        print(asm_code, file=self.out_file)
